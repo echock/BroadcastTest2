e13f03d31d6072613437ab62c311d9a6ab149ea1
echolin <893173260@qq.com>
2015-08-07 20:18:15 +0800

    first commit

---
+++ app/src/main/java/test/example/com/broadcasttest2/AnotherBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package test.example.com.broadcasttest2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by echo on 2015/8/7.
+ */
+public class AnotherBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"received in AnotherBroadcastReceiver",Toast.LENGTH_SHORT).show();
+    }
+}
